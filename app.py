c71763954760312b90cfb6b6dc95467075a99a46
TacoJimmy <t5669016@ntut.org.tw>
2022-07-07 01:04:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,11 @@
-# coding:utf-8
-import codecs
-import flask
-from flask import render_template
-
-from flask_script import Manager
-from livereload import Server
-from flask_apscheduler import APScheduler
-
-import readdata
-
-
-a = [0,0,0,0,0,0,0]
-app = flask.Flask(__name__)
-manager = Manager(app)
-
-
-class Config(object):
-    JOBS = [
-        {
-            'id': 'read_IAQ',
-            'func': '__main__:read_IAQ',
-            'args': (4, 5),
-            'trigger': 'interval',
-            'minutes': 1
-        },
-
-
-    ]
-
-def read_IAQ(a, b):
-    '''
-    PORT = '/dev/ttyS1'
-    ID = 1
-    payload = readdata.read_IAQ(PORT,ID)
-    '''
-    print ("001")
-    
-        
-    
-        
-if __name__ == '__main__':
-    
-    app.config.from_object(Config())
-    scheduler = APScheduler()
-    scheduler.init_app(app)
-    scheduler.start()
-    
-    
+import schedule  
+import time  
+  
+def job():  
+    print("I'm working...")  
+  
+schedule.every(10).seconds.do(job)  
+  
+while True:  
+    schedule.run_pending()  
+    time.sleep(1)  
