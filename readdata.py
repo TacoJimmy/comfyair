17fbbf547ca323a7c43c6ebf21e0d6713237dbf8
TacoJimmy <t5669016@ntut.org.tw>
2022-07-07 00:11:16 +0800

    Add files via upload

---
+++ readdata.py
@@ -0,0 +1,55 @@
+
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+import serial
+import modbus_tk
+import modbus_tk.defines as cst
+from modbus_tk import modbus_rtu
+import struct
+from _ast import If
+import codecs
+import statistics
+
+
+
+def read_IAQ(PORT,ID):
+    
+    IAQ_Value = [0,0,0,0,0]
+    try:
+        master = modbus_rtu.RtuMaster(serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+        master.set_timeout(5.0)
+        master.set_verbose(True)
+        IAQ = master.execute(ID, cst.READ_HOLDING_REGISTERS, 1, 5)
+        
+        
+        IAQ_Value[0] =  IAQ[0]
+        IAQ_Value[1] =  IAQ[1]
+        IAQ_Value[2] =  IAQ[2]
+        IAQ_Value[3] =  IAQ[3]
+        IAQ_Value[4] =  IAQ[4]
+        
+        master.close()
+        time.sleep(0.5)
+        return (IAQ_Value)
+
+    except:
+        IAQ_Value[0] =  0
+        IAQ_Value[1] =  0
+        IAQ_Value[2] =  0
+        IAQ_Value[3] =  0
+        IAQ_Value[4] =  0
+        
+        master.close()
+        time.sleep(0.5)
+        return (IAQ_Value) 
+     
+    
+if __name__ == '__main__':
+    while True:
+        PORT = '/dev/ttyS1'
+        ID = 1
+        
+        AC_IAQ = read_IAQ(PORT,ID)
+        print (AC_IAQ)
