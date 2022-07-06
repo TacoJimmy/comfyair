427ab59e1ab7cef562bec7158872ac87c04fc686
TacoJimmy <t5669016@ntut.org.tw>
2022-07-07 00:57:47 +0800

    Update fetmqtt.py

--- fetmqtt.py
+++ fetmqtt.py
@@ -1,7 +1,7 @@
 import paho.mqtt.client as mqtt
 import json  
 import time
-
+import readdata
 
 client = mqtt.Client()
 
@@ -11,9 +11,11 @@ client.username_pw_set("CuOkY4nZsqUmIfBkD6Eo","xxx")
 
 client.connect("thingsboard.cloud", 1883, 60)
 
-while True:
-    
-    payload = {'Temp' : 20}
-    print (json.dumps(payload))
-    client.publish("v1/devices/me/telemetry", json.dumps(payload))
-    time.sleep(5)
+
+if __name__ == '__main__':
+    while True:
+        
+        payload = {'Temp' : 20}
+        print (json.dumps(payload))
+        client.publish("v1/devices/me/telemetry", json.dumps(payload))
+        time.sleep(5)
