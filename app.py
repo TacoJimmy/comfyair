aca64fd6637e27f02af8254384457ddd97ef7dc1
TacoJimmy <t5669016@ntut.org.tw>
2022-07-07 01:16:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 import schedule  
 import time  
+import paho.mqtt.client as mqtt
+import json  
+import time
 import readdata
   
+client = mqtt.Client()
+client.username_pw_set("CuOkY4nZsqUmIfBkD6Eo","xxx")
+client.connect("thingsboard.cloud", 1883, 60)
+
 def job():  
     indoor_IAQ = readdata.read_IAQ('/dev/ttyS1',1)
-    print(indoor_IAQ)  
+    payload = {'CO2' : indoor_IAQ[0],'pm2_5' : indoor_IAQ[1],
+               'pm10' : indoor_IAQ[2],'temp' : indoor_IAQ[3],
+               'humid' : indoor_IAQ[4] }
+    print (json.dumps(payload))
+    client.publish("v1/devices/me/telemetry", json.dumps(payload))
+     
   
 schedule.every(10).seconds.do(job)  
   
